d83581a6c3541b623bbcdf715d83f98cdb32788d
VanessaMirand <vanessa.miranda51767@ucaldas.edu.co>
2024-04-19 23:13:39 -0500

    ejercicio del rectangulo

---
+++ prueba.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class prueba {
+    public static void main(String[] args) {
+        // Crear un objeto Rectangulo
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el ancho del rectángulo: ");
+        double ancho = scanner.nextDouble();
+        System.out.print("Ingrese el alto del rectángulo: ");
+        double alto = scanner.nextDouble();
+
+        ejercicio miRectangulo = new ejercicio(ancho,alto);
+
+
+        // Imprimir información sobre el rectángulo
+        miRectangulo.imprimirInformacion();
+        if (miRectangulo.esCuadrado()) {
+            System.out.println("El rectángulo es un cuadrado.");
+        } else {
+            System.out.println("El rectángulo no es un cuadrado.");
+        }
+    }
+}
+
